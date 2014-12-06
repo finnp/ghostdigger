5c1654b60a64f52d9ba2a92fb9b2dc543e5ecf4e
Finn <derfinn@gmail.com>
2014-12-06 14:50:28 +0100

    player

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
 window.onload = function () {
   var canvas = document.createElement('canvas') // <canvas id="game"></canvas>
   canvas.id = 'game'
-  var width = canvas.width = 640
-  var height = canvas.height = 640
+
+  canvas.width = 640
+  canvas.height = 640
+
+  var game = new Game(canvas.width, canvas.height)
+
   document.body.appendChild(canvas)
   if(typeof canvas == 'string') canvas = getElementById(canvas.id) // in case browser returns string
   var context = canvas.getContext('2d')
@@ -14,50 +18,41 @@ window.onload = function () {
   // |
   // V
   //  ----------> x
-
-  var tiles = []
-  var tilesX = 40
-  var tilesY = 40
-  for(var i = 0; i < tilesY; i++) {
-    tiles[i] = []
-    for(var j = 0; j < tilesX; j++) {
-      tiles[i][j] = 1
-    }
-  }
-
-
-  var offsetX = width / tiles[0].length
-  var offsetY = height / tiles.length
-
-  tiles[10][10] = 0
-  tiles[11][10] = 0
-  tiles[12][10] = 0
-  tiles[13][10] = 0
-  tiles[14][10] = 0
-  tiles[15][10] = 0
+  
+  var player = new Player(game)
+  player.setTilePos(3, 3)
 
   preload(startGame)
 
   function startGame(images) {
-    var ticker = requestAnimationFrame(draw)
-    function draw() {
-      for(var i = 0; i < tilesY; i++)
-        for(var j = 0; j < tilesX; j++) {
-          var tileSprite =  tiles[i][j] ? images.wall : images.floor
-          context.drawImage(tileSprite,
-            Math.floor(offsetX * j),
-            Math.floor(offsetY * i),
-            Math.floor(offsetX),
-            Math.floor(offsetY)
-          )
-       }
-     }
+    console.log('startGame')
+    requestAnimationFrame(draw)
+    function draw(timestamp) {
+      console.log('draw')
+      for(var i = 0; i < game.tilesY; i++) {
+        for(var j = 0; j < game.tilesX; j++) {
+            var tileSprite =  game.tiles[i][j] ? images.wall : images.floor
+            context.drawImage(tileSprite,
+              Math.floor(game.offsetX * j),
+              Math.floor(game.offsetY * i),
+              Math.floor(game.offsetX),
+              Math.floor(game.offsetY)
+            )
+         }
+      }
+      console.log(player.pos)
+      context.drawImage(images.player,
+        player.pos.x, player.pos.y, game.offsetX, game.offsetY
+      )
+      // requestAnimationFrame(draw)
+    }
   }
 
   function preload(cb) {
     var images =  {
      floor: 'img/floor.png',
-     wall: 'img/wall.png'
+     wall: 'img/wall.png',
+     player: 'img/player.png'
     }
     var loadedImages = {}
     var n = Object.keys(images).length
@@ -73,5 +68,63 @@ window.onload = function () {
       n--
       if(n === 0) cb(loadedImages)
     }
+  }
+}
+
+//------CLASSES------//
+
+function Game(width, height) {
+  var tiles = []
+  var tilesX = 40
+  var tilesY = 40
+  for(var i = 0; i < tilesY; i++) {
+    tiles[i] = []
+    for(var j = 0; j < tilesX; j++) {
+      tiles[i][j] = 1
+    }
+  }
+
+  tiles[10][10] = 0
+  tiles[11][10] = 0
+  tiles[12][10] = 0
+  tiles[13][10] = 0
+  tiles[14][10] = 0
+  tiles[15][10] = 0
+  
+  this.width = width
+  this.height = height
+
+  this.offsetX = this.width / tiles[0].length
+  this.offsetY = this.height / tiles.length
+
+  this.tilesX = tilesX
+  this.tilesY = tilesY
+
+  this.tiles = tiles
+}
+
+
+function Player(game) {
+  this.game = game
+  this.pos = {
+    x: 10,
+    y: 10
+  }
+  this.speed = 1
+}
+
+Player.prototype.up = function () {
+  this.pos.y--
+}
+
+Player.prototype.setTilePos = function (tileX, tileY) {
+  this.pos.x = tileX*this.game.offsetX
+  this.pos.y = tileY*this.game.offsetY
+}
+
+Player.prototype.getTilePos = function () {
+  return {
+    x: this.pos.x / this.offsetX,
+    y: this.pos.y / this.offsetY
   }
 }
